e297a58d88712bbfb507c8ca1a09558ceba5dff2
Jatin Kapur <jkapur.work@gmail.com>
2019-10-22 12:49:11 -0500

    first commit

---
+++ giraffe.cpp
@@ -0,0 +1,25 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main() 
+{ 
+    string characterName = "John"; 
+    int characterAge; 
+    characterAge = 35; 
+    int newage; 
+   
+
+    cout << "There was once a man named "  << characterName << endl; 
+    cout << "His age was " << characterAge << endl; 
+    cout << "He grew and lived to be a very very old man..." << newage << endl; 
+
+    cout << "Hello World" << endl; 
+
+
+        int age; 
+        cout << "Enter your age: "; 
+        cin >> age; 
+
+    return 0; 
+}   
